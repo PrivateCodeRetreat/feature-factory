366fe6bcf9488ff7df9a9210ea3ec0db6c5a4e56
Nitsan Avni <nitsanav@gmail.com>
2023-11-10 20:38:23 +0100

    chat impl get_age

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -9,7 +9,8 @@ from approvaltests.storyboard import Storyboard
 class GameOfLife:
     def __init__(self):
         self.alive_cells = set()
-    
+        self.cell_ages = {}
+
     def __str__(self) -> str:
         height = self.get_max_y() + 1
         width = self.get_max_x() + 1
@@ -23,15 +24,17 @@ class GameOfLife:
 
     def set_alive(self, x, y):
         self.alive_cells.add((x, y))
+        self.cell_ages[(x, y)] = self.cell_ages.get((x, y), 0) + 1
 
     def is_alive(self, x, y):
         return (x, y) in self.alive_cells
 
     def get_age(self, x, y):
-        return 0
+        return self.cell_ages.get((x, y), 0)
 
     def advance(self):
         new_alive_cells = set()
+        new_cell_ages = {}
         neighbor_offsets = [(-1, -1), (-1, 0), (-1, 1),
                             ( 0, -1),          ( 0, 1),
                             ( 1, -1), ( 1, 0), ( 1, 1)]
@@ -42,11 +45,14 @@ class GameOfLife:
         for cell in potential_cells:
             alive_neighbors = sum((neighbor in self.alive_cells) for neighbor in 
                                   [(cell[0] + dx, cell[1] + dy) for dx, dy in neighbor_offsets])
+            current_age = self.cell_ages.get(cell, 0)
 
             if alive_neighbors == 3 or (alive_neighbors == 2 and cell in self.alive_cells):
                 new_alive_cells.add(cell)
+                new_cell_ages[cell] = current_age + 1
 
         self.alive_cells = new_alive_cells
+        self.cell_ages = new_cell_ages
 
 
 def create_glider_at(game,x,y):
